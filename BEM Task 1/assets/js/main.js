5136b1f259178bcc47acab0a612e4803a113db4d
rohan mahadik <rohan mahadik>
2022-04-22 14:13:46 +0530

    adding task

---
+++ BEM Task 1/assets/js/main.js
@@ -0,0 +1,7 @@
+< script >
+    $(document).ready(function() {
+        $(".fa-search").click(function() {
+            $(".vidhirriheader__aboutus--searchfield").toggle();
+        });
+    }); <
+/script>
